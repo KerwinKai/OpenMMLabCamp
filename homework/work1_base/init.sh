f42ab4a01152612187573fbaa8eed94ef2af4b92
KerwinKai <kerwincheng@foxmail.com>
2023-02-04 21:53:15 +0800

    Update init.sh

--- homework/work1_base/init.sh
+++ homework/work1_base/init.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
+export WORK_KERWIN=${PWD}
 export HOME_KERWIN="/HOME/scz0b6e"
 export DATASET_KERWIN="/data/run01/scz0b6e"
-cd ${DATASET_KERWIN}
+cd $DATASET_KERWIN
 unzip flower_dataset.zip
-cd ${HOME_KERWIN}
+python "$WORK_KERWIN"/split_data.py flower_dataset/ flower_dataset_split/
+
+# shellcheck disable=SC2164
+cd $WORK_KERWIN
