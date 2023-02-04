e5cffde758ab582f2c72e4c7c44529d220e134e3
KerwinKai <kerwincheng@foxmail.com>
2023-02-04 22:27:35 +0800

    add

--- homework/work1_base/run.sh
+++ homework/work1_base/run.sh
@@ -5,9 +5,9 @@ module load gcc/7.3
 
 source activate opennmmlab_mmclassification
 export PYTHONUNBUFFERED=1
-
+export PATH_MMCLS="/HOME/scz0b6e/OpenMMLabCamp/mmclassification"
+export PATH_WORK1_BASE="/HOME/scz0b6e/OpenMMLabCamp/homework/work1_base"
 N_GPUS=8
 python -m torch.distributed.launch \
         --nproc_per_node=${N_GPUS} \
-        tools/train.py configs/resnet18/resnet18_b32_flower.py --work-dir
-        work/resnet18_b32_flower --launcher py
+        "$PATH_MMCLS"/tools/train.py "$PATH_WORK1_BASE"/resnet18_b32_flower.py --work-dir resnet18_b32_flower --launcher py
