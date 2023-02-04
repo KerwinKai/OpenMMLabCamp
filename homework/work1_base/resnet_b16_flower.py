b063a3fcfeea923e4ff0ad64602cc6b870179880
KerwinKai <kerwincheng@foxmail.com>
2023-02-04 22:14:57 +0800

    Update resnet_b16_flower.py

--- homework/work1_base/resnet_b16_flower.py
+++ homework/work1_base/resnet_b16_flower.py
@@ -107,6 +107,6 @@ workflow = [('train', 1)]
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='step', step=[1])
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+runner = dict(type='EpochBasedRunner', max_epochs=10)
 work_dir = 'work_dirs/flower'
 gpu_ids = [0]
