0c573d2b9b9ed6f92e52b908ee01319779948626
KerwinKai <kerwincheng@foxmail.com>
2023-02-05 14:14:53 +0800

    Update run.sh

--- homework/work1_base/run.sh
+++ homework/work1_base/run.sh
@@ -10,7 +10,6 @@ export PATH_WORK1_BASE="/HOME/scz0b6e/OpenMMLabCamp/homework/work1_base"
 export DATASET_WORK1_BASE="/HOME/scz0b6e/run/flower_dataset_split"
 cd $PATH_WORK1_BASE
 mkdir resnet18_b32_flower
-cd $DATASET_WORK1_BASE
 python "$PATH_MMCLS"/tools/train.py \
         "$PATH_WORK1_BASE"/resnet_b32_flower.py \
         --work-dir "$PATH_WORK1_BASE"/resnet18_b32_flower
