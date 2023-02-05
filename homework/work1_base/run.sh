2036d649ba9805582f0042a2359c877e0ab9c4e3
KerwinKai <kerwincheng@foxmail.com>
2023-02-05 13:57:45 +0800

    Update run.sh

--- homework/work1_base/run.sh
+++ homework/work1_base/run.sh
@@ -7,9 +7,10 @@ source activate opennmmlab_mmclassification
 export PYTHONUNBUFFERED=1
 export PATH_MMCLS="/HOME/scz0b6e/mmclassification"
 export PATH_WORK1_BASE="/HOME/scz0b6e/OpenMMLabCamp/homework/work1_base"
+export DATASET_WORK1_BASE="/HOME/scz0b6e/run/flower_dataset_split"
 cd $PATH_WORK1_BASE
 mkdir resnet18_b32_flower
-
+cd $DATASET_WORK1_BASE
 python "$PATH_MMCLS"/tools/train.py \
         "$PATH_WORK1_BASE"/resnet_b32_flower.py \
-        --work-dir resnet18_b32_flower
+        --work-dir "$PATH_WORK1_BASE"/resnet18_b32_flower
